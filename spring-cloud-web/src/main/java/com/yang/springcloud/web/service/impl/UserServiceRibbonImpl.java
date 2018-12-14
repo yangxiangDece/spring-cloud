1848cc382cdea875ecdbc20795dd74c9e8cfa123
YangXiang <yangxiang@chuangjialive.com>
2018-12-14 19:30:33 +0800

    Spring Cloud

--- spring-cloud-web/src/main/java/com/yang/springcloud/web/service/impl/UserServiceRibbonImpl.java
+++ spring-cloud-web/src/main/java/com/yang/springcloud/web/service/impl/UserServiceRibbonImpl.java
@@ -1,9 +1,13 @@
 package com.yang.springcloud.web.service.impl;
 
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
+import com.netflix.hystrix.contrib.javanica.command.AsyncResult;
 import com.yang.springcloud.web.service.UserServiceRibbon;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
+
+import java.util.concurrent.Future;
 
 @Service(value = "userServiceRibbon")
 public class UserServiceRibbonImpl implements UserServiceRibbon {
@@ -21,5 +25,22 @@ public class UserServiceRibbonImpl implements UserServiceRibbon {
 
     private String helloFallback(String id){
         return "hello...error.."+id;
+    }
+
+    /**
+     * Observable<String> hotObservable=new UserCommand(restTemplate,1L).observe();
+     * Observable<String> coldObservable=new UserCommand(restTemplate,1L).toObservable();
+     * observe()：返回的是一个Hot Observable，该命令会在observe()
+     */
+
+    //异步执行
+    @HystrixCommand
+    public Future<String> getStringByIdAsync(final String id){
+        return new AsyncResult<String>() {
+            @Override
+            public String invoke() {
+                return restTemplate.getForObject("http://spring-cloud-provider/hello?id={1}",String.class,id);
+            }
+        };
     }
 }
